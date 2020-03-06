c2fc1a3a6e7a7d6897c97919f493de02d268952e
Zach Wahrer <zachtheclimber@gmail.com>
2020-03-06 11:00:11 -0700

    Rename config file for Dockerization

---
+++ includes/rfas_config.pl
@@ -0,0 +1,4 @@
+# Config file for the Routesetting Feedback Script
+
+$mysql_username = 'root';
+$mysql_password = 'password';
--- includes/rfas_config.sample.pl
+++
@@ -1,4 +0,0 @@
-# Config file for the Routesetting Feedback Script
-
-$mysql_username = 'YOUR_MYSQL_USERNAME';
-$mysql_password = 'YOUR_MYSQL_PASSWORD';
